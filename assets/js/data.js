afbf83f5547eba6e34a545a8abaa56985f611c15
Felipe Renan <feliperenanrf23@gmail.com>
2019-08-21 00:53:01 -0300

    Tela de produtos

---
+++ assets/js/data.js
@@ -0,0 +1,25 @@
+{
+	products: [
+		{
+			name: 'Agenda Fireux',
+			description: 'Descrição simples contando um pouco sobre a agenda',
+			link_page: '/agenda',
+			link_google_play: 'sei la o link disso',
+			icon_url: './assets./images/logo.png'
+		},
+		{
+			name: 'App Novo',
+			description: 'Descrição simples contando um pouco sobre o app novo',
+			link_page: '/appNovo',
+			link_google_play: 'sei la o link disso',
+			icon_url: './assets./images/logo.png'
+		},
+		{
+			name: 'App Velho',
+			description: 'Descrição simples contando um pouco sobre o app velho',
+			link_page: '/appVelho',
+			link_google_play: 'sei la o link disso',
+			icon_url: './assets./images/logo.png'
+		}
+	];
+}
